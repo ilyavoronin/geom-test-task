620813185a1dfd99d0cfb724e2026c5d90fe9877
Ilya Voronin <voroninilya115@gmail.com>
2020-02-11 22:04:29 +0300

    implement operators

--- src/vec.cpp
+++ src/vec.cpp
@@ -1,2 +1,29 @@
-#include "../include/vec.h"
+#include "vec.h"
 
+vec& vec::operator+=(const vec& ot) {
+    x += ot.x;
+    y += ot.y;
+    return *this;
+}
+
+vec vec::operator+(const vec& ot) {
+    return vec(x + ot.x, y + ot.y);
+}
+
+vec& vec::operator-=(const vec& ot) {
+    x -= ot.x;
+    y -= ot.y;
+    return *this;
+}
+
+vec vec::operator-(const vec& ot) {
+    return vec(x - ot.x, y - ot.y);
+}
+
+double vec::operator*(const vec& ot) {
+    return x * ot.x + y * ot.y;
+}
+
+double vec::operator%(const vec& ot) {
+    return x * ot.y - y * ot.x;
+}
